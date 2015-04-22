032d2a30dc0dc0adeec8cccafb39f55b105a1d5c
james <whelan.james@gmail.com>
2015-04-22 19:02:26 -0400

    rangeand while

--- PyLearn/Exercise Files/Other/RangeAndWhile.py
+++ PyLearn/Exercise Files/Other/RangeAndWhile.py
@@ -1,3 +1,17 @@
-for item in range(10):
+for item in range(10): # (n) prints out n times
     print (item)
     print ('james is awesome!!!')
+print ('end lesson *****************')
+for ritem in range(5,12): # when doing a range (a,b) where a is start and b is finish
+    print(ritem)
+print ('end lesson *****************')
+for increitem in range(10,60,5):# when range (a,b,c) where a is start and b is finish and c is the increment
+    print(increitem)
+print ('end lesson *****************')
+
+mynumber = 5
+
+while mynumber < 10:
+    print("that's my number")
+    mynumber += 1   # a += b is essentially the same as a = a + b, except that a is evaluated twice.
+
