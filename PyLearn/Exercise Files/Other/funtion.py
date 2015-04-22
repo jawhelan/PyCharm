30358e61363ee59162a4ec2be978a7d02170a3ba
jawhelan <whelan.james@gmail.com>
2015-04-22 15:59:11 -0400

    arguments kwargs

---
+++ PyLearn/Exercise Files/Other/funtion.py
@@ -0,0 +1,13 @@
+def something():
+    print (" my funtion name is something!")
+
+something()
+something()
+
+
+def bitcointousd(btc):
+    amount = btc * 527
+    print(amount)
+
+bitcointousd(2.32)
+bitcointousd(3.82)
