c4d0313b7fa0e693f9a05587173b66e6d0bbd6e1
jawhelan <whelan.james@gmail.com>
2015-04-22 16:21:07 -0400

    return examples

---
+++ PyLearn/Exercise Files/Other/return.py
@@ -0,0 +1,9 @@
+def allowed_dating_are(myage):
+    girls_age = myage/2 + 7
+    return girls_age
+
+def askFloat(prompt):
+    return float(input(prompt))
+x = askFloat("Enter a floating-point number: ")
+james_limit = allowed_dating_are(x)
+print("James' you can only date girls over", james_limit)
