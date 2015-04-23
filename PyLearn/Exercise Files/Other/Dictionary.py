61a2bae2e462baa393b60d00485dfeae60a0ebd8
james <whelan.james@gmail.com>
2015-04-22 22:00:36 -0400

    update

---
+++ PyLearn/Exercise Files/Other/Dictionary.py
@@ -0,0 +1,6 @@
+coworkers = {"Damir":"is a QA guy", "Chris":"does all the code","Sheldon":"not sure what he does","Damir":"a QA guy",}
+                                        # dictionary is like a key-value pair set, will only print one key
+print(coworkers)
+
+for k,v in coworkers.items():
+    print(k,v)
