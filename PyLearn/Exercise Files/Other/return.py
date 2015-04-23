2af81e6c9c781f29310f9b767bf57e61c7da8882
james <whelan.james@gmail.com>
2015-04-22 20:12:18 -0400

    update

--- PyLearn/Exercise Files/Other/return.py
+++ PyLearn/Exercise Files/Other/return.py
@@ -2,8 +2,8 @@ def allowed_dating_are(myage):
     girls_age = myage/2 + 7
     return girls_age
 
-def askFloat(prompt):
-    return float(input(prompt))
-x = askFloat("Enter a floating-point number: ")
+def askFloat(prompt):   # create a method funtion called ask float and create an argument called prompt
+    return float(input(prompt))   # return promt as an input and convert to float
+x = askFloat("Enter a floating-point number: ")   # create a varible called x and
 james_limit = allowed_dating_are(x)
 print("James'  can only date girls over", james_limit)
